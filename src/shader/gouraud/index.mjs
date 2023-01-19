ebcddf5fe72adf98f273beafb4d77cb1ae734116
icey <icey@icey.tech>
2023-01-19 15:27:35 +0800

    Merge math.glsl back

--- src/shader/gouraud/index.mjs
+++ src/shader/gouraud/index.mjs
@@ -1,7 +1,6 @@
 import { createShaderProgram } from '../../glx/shader_program.mjs'
 
-import mathLib from '../include/math.glsl?raw'
 import vsSource from './gouraud.vert?raw'
 import fsSource from './gouraud.frag?raw'
 
-export const createGouraudShader = gl => createShaderProgram(gl, mathLib + vsSource, fsSource)
+export const createGouraudShader = gl => createShaderProgram(gl, vsSource, fsSource)
