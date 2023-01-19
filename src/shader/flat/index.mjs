11023b89bb24d4755fcb37d9511c8f31e3cb41b0
icey <icey@icey.tech>
2023-01-19 15:23:47 +0800

    Remove useless `console.log`

--- src/shader/flat/index.mjs
+++ src/shader/flat/index.mjs
@@ -2,7 +2,4 @@ import { createShaderProgram } from '../../glx/shader_program.mjs'
 import vsSource from './flat.vert?raw'
 import fsSource from './flat.frag?raw'
 
-console.log(vsSource)
-console.log(fsSource)
-
 export const createFlatShader = gl => createShaderProgram(gl, vsSource, fsSource)
